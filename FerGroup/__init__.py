9eb5a6e2560bad2fe1420b25be69c7301ecbecfa
Tona <tonamatos@gmail.com>
2025-04-15 13:37:18 -0400

    Refactored into module

---
+++ FerGroup/__init__.py
@@ -0,0 +1,5 @@
+# FerGroup/__init__.py
+
+__version__ = "1.0.0"
+
+from Fer_G import FerGroup
---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+  name="FerGroup",
+  version="1.0.0",
+  description="The comprehensive framework for the study of Amoeba graphs and the Fer group",
+  author="Tonatiuh Matos-Wiederhold",
+  packages=find_packages(),
+  include_package_data=True,
+  python_requires='>=3.7',
+  install_requires=[
+    'networkx',
+    'sympy.combinatorics'
+  ],
+)
